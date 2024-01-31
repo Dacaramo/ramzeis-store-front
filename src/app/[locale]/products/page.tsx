73551b08cb069a74000020f1134eee79982aed7c
Dacaramo <dankramirez@outlook.com>
2024-01-31 00:30:32 -0500

    refactor: Removed useless fragment

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -11,29 +11,28 @@ interface Props {
 
 const ProductsPage: FC<Props> = ({ params: { locale } }) => {
   const t = useTranslations('products-page.filter-section');
+
   return (
-    <>
-      <ProductGrid
-        products={testProducts}
-        filterSectionTranslations={{
-          'price-filter': {
-            'general-label': t('price-filter.general-label'),
-            'max-price-label': t('price-filter.max-price-label'),
-            'max-price-placeholder': t('price-filter.max-price-placeholder'),
-            'min-price-label': t('price-filter.min-price-label'),
-            'min-price-placeholder': t('price-filter.min-price-placeholder'),
-          },
-          'price-sort': {
-            'general-label': t('price-sort.general-label'),
-            'asc-button-text': t('price-sort.asc-button-text'),
-            'desc-button-text': t('price-sort.desc-button-text'),
-          },
-          'color-filter': {
-            'general-label': t('color-filter.general-label'),
-          },
-        }}
-      />
-    </>
+    <ProductGrid
+      products={testProducts}
+      filterSectionTranslations={{
+        'price-filter': {
+          'general-label': t('price-filter.general-label'),
+          'max-price-label': t('price-filter.max-price-label'),
+          'max-price-placeholder': t('price-filter.max-price-placeholder'),
+          'min-price-label': t('price-filter.min-price-label'),
+          'min-price-placeholder': t('price-filter.min-price-placeholder'),
+        },
+        'price-sort': {
+          'general-label': t('price-sort.general-label'),
+          'asc-button-text': t('price-sort.asc-button-text'),
+          'desc-button-text': t('price-sort.desc-button-text'),
+        },
+        'color-filter': {
+          'general-label': t('color-filter.general-label'),
+        },
+      }}
+    />
   );
 };
 
